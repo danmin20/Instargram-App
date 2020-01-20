9ef92c762153935ce665e6d21bdfba5d5857f637
Lee Jeong Min <danmin20@khu.ac.kr>
2020-01-21 01:06:06 +0900

    MessageNavigation

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -2,11 +2,13 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import TabNavigation from "./TabNavigation";
 import PhotoNavigation from "./PhotoNavigation";
+import MessageNavigation from "./MessageNavigation";
 
 const MainNavigation = createStackNavigator(
   {
     TabNavigation,
-    PhotoNavigation
+    PhotoNavigation,
+    MessageNavigation
   },
   {
     headerMode: "none"
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,14 +1,36 @@
+import React from "react"
 import { createBottomTabNavigator } from "react-navigation-tabs";
 import Home from "../screens/Tab/Home";
 import Search from "../screens/Tab/Search";
 import Notifications from "../screens/Tab/Notifications";
 import Profile from "../screens/Tab/Profile";
 import { View } from "react-native";
+import { createStackNavigator } from "react-navigation-stack";
+import MessagesLink from "../components/MessagesLink";
+
+const stackFactory = (initialRoute, customConfig) =>
+  createStackNavigator({
+    initialRoute: {
+      screen: initialRoute,
+      navigationOptions: { ...customConfig }
+    }
+  });
 
 export default createBottomTabNavigator(
   {
-    Home,
-    Search,
+    Home: {
+      screen: stackFactory(Home, {
+        title: "Home",
+        headerTitleAlign: "center",
+        headerRight: () => <MessagesLink />
+      })
+    },
+    Search: {
+      screen: stackFactory(Search, {
+        title: "Search",
+        headerTitleAlign: "center"
+      })
+    },
     Add: {
       screen: View,
       navigationOptions: {
@@ -16,8 +38,18 @@ export default createBottomTabNavigator(
           navigation.navigate("PhotoNavigation")
       }
     },
-    Notifications,
-    Profile
+    Notifications: {
+      screen: stackFactory(Notifications, {
+        title: "Notifications",
+        headerTitleAlign: "center"
+      })
+    },
+    Profile: {
+      screen: stackFactory(Profile, {
+        title: "Profile",
+        headerTitleAlign: "center"
+      })
+    }
   },
   {
     headerMode: "none"
