8dd85b9faf00c94654cb966a9d84441d664bbb7e
Lee Jeong Min <danmin20@khu.ac.kr>
2020-01-22 00:05:40 +0900

    home screen

--- screens/Tab/Home.js
+++ screens/Tab/Home.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { useQuery } from "react-apollo-hooks";
+import { gql } from "apollo-boost";
+import Loader from "../../components/Loader";
+import { ScrollView, RefreshControl } from "react-native";
 
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
 const View = styled.View`
   justify-content: center;
   align-items: center;
@@ -9,8 +42,26 @@ const View = styled.View`
 
 const Text = styled.Text``;
 
-export default () => (
-  <View>
-    <Text>Home</Text>
-  </View>
-);
+export default () => {
+  const [refreshing, setRefreshing] = useState(false);
+  const { loading, data, refetch } = useQuery(FEED_QUERY);
+  const refresh = async () => {
+    try {
+      setRefreshing(true);
+      await refetch();
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setRefreshing(false);
+    }
+  };
+  return (
+    <ScrollView
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={refresh} />
+      }
+    >
+      {loading ? <Loader /> : <Text>Hello</Text>}
+    </ScrollView>
+  );
+};
